369b8dae0e6d5c913e686be2533a08a3576c3869
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2025-12-29 14:06:17 +0000

    Complete task-2

--- index.js
+++ index.js
@@ -2,6 +2,27 @@
 
 // 2. Написати код, який запитує через prompt пароль користувача, порівнює його з тим, який у неї збережений (захардкоджений у константі). Якщо пароль співпав - виводить на консоль “Ви успішно увійшли в систему”, якщо ні - продовжує запитувати пароль і перевіряти, поки не введуть правильно (за допомогою вічного цикла)
 
+const Password_Stored = "Anna123";
+let passwordInput = prompt("Enter your password:");
+
+while (passwordInput !== Password_Stored) {
+  passwordInput = prompt("Incorrect password. Please try again:");
+}
+console.log("You have successfully logged in.");
+
+// or
+
+while (true) {
+  let passwordInput = prompt("Enter your password:");
+
+  if (passwordInput === Password_Stored) {
+    console.log("You have successfully logged in.");
+    break;
+  } else {
+    alert("Wrong password!");
+  }
+}
+
 // 3. Обчислити сумму чисел від 1 до 100 і вивести її в консоль
 
 // 4. Вивести числа від 10 до 50, які кратні 5.
