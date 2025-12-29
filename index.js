6d002d9706b1947a6767748fe8897f33ac56a753
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2025-12-29 14:12:05 +0000

    Complete task-7

--- index.js
+++ index.js
@@ -18,3 +18,12 @@
 // 6. Намалювати пустий квадрат з діагоналлю (зліва зверху - до права низу)
 
 // 7. Задачка з *: Зробити квадрат зі зворотньою діагоналлю (з правого верхнього до лівого нижнього)
+
+const Square_Size_2 = 5;
+console.log("-".repeat(Square_Size_2 + 6));
+for (let i = 1; i < Square_Size_2; i++) {
+  console.log(
+    "|" + "  ".repeat(Square_Size_2 - i - 1) + "." + "  ".repeat(i) + "|"
+  );
+}
+console.log("-".repeat(Square_Size_2 + 6));
